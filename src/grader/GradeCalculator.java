1c21a89218cf95fec22dcb6de86a042fcb3fb1ab
AviM <129541673+avimtcg@users.noreply.github.com>
2025-09-28 03:12:39 -0400

    added trey's grade calculator

---
+++ src/grader/GradeCalculator.java
@@ -0,0 +1,27 @@
+/*
+Author: Trey
+ */
+
+package grader;
+
+public class GradeCalculator {
+    public double calculate(String[] scores, String[] weights) {
+        double total = 0, totalWeight = 0;
+        for (int i = 0; i < scores.length; i++) {
+            double score = parse(scores[i]);
+            double weight = parse(weights[i]);
+            total += score * weight;
+            totalWeight += weight;
+        }
+        return total / totalWeight * 100;
+    }
+
+    private double parse(String str) {
+        if (str.contains("/")) {
+            String[] parts = str.split("/");
+            return Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]);
+        }
+        double num = Double.parseDouble(str.replace("%", ""));
+        return str.contains("%") ? num / 100 : num > 1 ? num / 100 : num;
+    }
+}
